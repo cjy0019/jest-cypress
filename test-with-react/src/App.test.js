029419d109853ea4d924ab4fd0b5295fd33eeaff
JYCHO <cjy0029@naver.com>
2022-02-04 23:53:24 +0900

    feat : h1 test 추가

--- test-with-react/src/App.test.js
+++ test-with-react/src/App.test.js
@@ -1,6 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('Rendering Application', () => {
-  render(<App />);
+describe('Redering Application', () => {
+  test('is title exist', () => {
+    render(<App />);
+    const title = screen.getByRole('heading');
+    expect(title).toHaveTextContent('TO-DO-LIST');
+  });
 });
